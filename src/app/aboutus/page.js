6161eb723462c806d192f3da6437340d7f86bf4a
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2024-10-09 13:02:05 +0530

    About Us Update

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -132,51 +132,30 @@ export default function AboutUs() {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center">
-          <div className="grid grid-cols-1 pb-6 text-center lg:mt-24 mt-16">
-            <h3 className="mb-4 md:text-2xl text-xl font-medium">
-              Research Interests
-            </h3>
-            <div className="lg:col-span-7">
-              <div className="bg-gray-50 dark:bg-slate-800 shadow dark:shadow-gray-800 p-6 rounded-xl mx-16 text-left">
-                <h5 className="text-lg font-semibold border-b border-gray-100 dark:border-gray-700 pb-3 mb-3">
-                  Project Info :
-                </h5>
-                <dl className="grid grid-cols-12 mb-0">
-                  <dt className="md:col-span-4 col-span-5 mt-2">Client :</dt>
-                  <dd className="md:col-span-8 col-span-7 mt-2 text-slate-400">
-                    Calvin Carlo
-                  </dd>
-
-                  <dt className="md:col-span-4 col-span-5 mt-2">Category :</dt>
-                  <dd className="md:col-span-8 col-span-7 mt-2 text-slate-400">
-                    Web Design
-                  </dd>
-
-                  <dt className="md:col-span-4 col-span-5 mt-2">Date :</dt>
-                  <dd className="md:col-span-8 col-span-7 mt-2 text-slate-400">
-                    23rd Sep, 2021
-                  </dd>
-
-                  <dt className="md:col-span-4 col-span-5 mt-2">Website :</dt>
-                  <dd className="md:col-span-8 col-span-7 mt-2 text-slate-400">
-                    www.yourdomain.com
-                  </dd>
-
-                  <dt className="md:col-span-4 col-span-5 mt-2">Location :</dt>
-                  <dd className="md:col-span-8 col-span-7 mt-2 text-slate-400">
-                    3/2/64 Mongus Street, UK
-                  </dd>
-                </dl>
-              </div>
-            </div>
+        <section className="relative md:py-24 py-16">
+  <div className="container relative">
+    <div>
+      <div className="grid grid-cols-1 pb-6 text-center lg:mt-24 mt-16">
+        <h3 className="mb-4 md:text-2xl text-xl font-medium">
+          Research Interests
+        </h3>
+        <div className="lg:col-span-7">
+          <div className="bg-gray-50 dark:bg-slate-800 shadow dark:shadow-gray-800 p-6 rounded-xl mx-16 text-left">
+            <ul className="w-full list-disc pl-6 mx-16"  style={{ textAlign: "left" }}>
+              <li className="mt-2">
+                Sustainable Design and Construction
+              </li>
+              <li className="mt-2">Construction Project Management</li>
+              <li className="mt-2">Green Buildings and Materials</li>
+              <li className="mt-2">Marine Pollution and Environment</li>
+            </ul>
           </div>
-
-          {/* <p className="text-slate-400 dark:text-white/60 max-w-xl mx-auto">
-            This is just a simple text made for this unique and awesome
-            template, you can replace it with any text.
-          </p> */}
         </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
         <ContactUs />
       </section>
